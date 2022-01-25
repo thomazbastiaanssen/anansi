77f42fb895edd0ceb16f06987580b83bfc3e00e4
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2022-01-25 12:14:36 +0000

    Testing the waters with documentation

--- R/anansi.R
+++ R/anansi.R
@@ -1,8 +1,15 @@
+#' Calculate an association network
+#' @param web An anansi web object, containing two tables with omics data and a dictionary that links them.
+#' @param method Correlation method. Method="pearson" is the default value. The alternatives to be passed to cor are "spearman" and "kendall".
+#' @param groups A categorical or continuous value necessary for differential correlations. Typically a state or treatment score.
+#' @param adjust.method Method to adjust p-values for multiple comparisons. Method="BH" is the default value. See p.adjust in the base R stats package.
+#' @param verbose A boolean. Toggles whether to print diagnostic information while running. Useful for debugging errors on large datasets.
+
 anansi = function(web, method = "pearson", groups, adjust.method = "BH", verbose = T){
-  
+
   if(verbose){print("Running annotation-based correlations")}
   out_cors  = anansiCorTestByGroup(web = web, method = method, groups = groups, adjust.method = adjust.method, verbose = verbose)
-  
+
   if(verbose){print("Fitting models for differential correlation testing")}
   out_models = anansiDiffCor(web = web, groups = groups, adjust.method = adjust.method)
 
