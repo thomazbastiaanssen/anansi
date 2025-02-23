f943c29cbbd9a7fbce22b76de02fc8769ef8b2d2
Giulio Benedetti <giuliobene2000@gmail.com>
2025-02-23 14:25:19 +0200

    Update plotAnansi

--- R/plotAnansi.R
+++ R/plotAnansi.R
@@ -1,40 +1,36 @@
 #' Dissociation plot
 #'
 #' \code{plotAnansi} generates a standard dissociation plot from the output of
-#' \code{\link{getAnansi}} in the long format. It provides a convenient way to
+#' \code{\link{getAnansi}} in the table format. It provides a convenient way to
 #' visually assess relevant results from the anansi analysis.
 #'
-#' @param df a \code{data.frame} object output of \code{\link{getAnansi}} in
-#'   the long format.
-#'
-#' @param x \code{Character scalar}. Specifies the type of coefficient to
-#' show in the plot. One of \code{"r.values"}, \code{"r.squared"} and
-#'   \code{"q.values"}. (Default: \code{"r.values"})
-#'
+#' @param x a \code{data.frame} object output of \code{\link{getAnansi}} in
+#'   the table format.
+#'  
 #' @param association.type \code{Character scalar}. Specifies the type of
 #' association to show in the plot. One of \code{"disjointed"},
-#'   \code{"emergent"} and \code{"full"}. (Default: \code{"disjointed"})
+#'   \code{"emergent"} and \code{"full"}. (Default: \code{NULL})
 #'
 #' @param model.var \code{Character scalar}. Specifies the name of a variable
 #' in the anansi model. It is relevant only when \code{association.type} is
 #'   \code{"disjointed"} or \code{"emergent"}. (Default: \code{NULL})
 #'
-#' @param signif.threshold \code{Numeric scalar}. Specifies the significance
-#'   threshold to show in the plot. (Default: \code{NULL})
+#' @param signif.threshold \code{Numeric scalar}. Specifies the threshold to
+#'   mark the significance of \code{association.type}. (Default: \code{NULL})
 #'
 #' @param colour_by \code{Character scalar}. Specifies the name of a column in
-#'   \code{df} by which points should be coloured. (Default: \code{NULL})
+#'   \code{x} by which points should be coloured. (Default: \code{NULL})
 #'
 #' @param color_by \code{Character scalar}. Alias to \code{colour_by}.
 #'
 #' @param fill_by \code{Character scalar}. Specifies the name of a column in
-#'   \code{df} by which points should be filled (Default: \code{NULL})
+#'   \code{x} by which points should be filled (Default: \code{NULL})
 #'
 #' @param size_by \code{Character scalar}. Specifies the name of a column in
-#'   \code{df} by which points should be sized (Default: \code{NULL})
+#'   \code{x} by which points should be sized (Default: \code{NULL})
 #'
 #' @param shape_by \code{Character scalar}. Specifies the name of a column in
-#'   \code{df} by which points should be shaped (Default: \code{NULL})
+#'   \code{x} by which points should be shaped (Default: \code{NULL})
 #'
 #' @param x_lab \code{Character scalar}. Specifies the label of the x axis.
 #'   (Default: \code{"cor"})
@@ -50,13 +46,12 @@
 #' \code{plotAnansi} provides a standardised method to visualise the results
 #' of anansi by means of a differential association plot. The input for this
 #' function should be generated from \code{\link{getAnansi}} or
-#' \code{\link{anansi}}, with \code{return.format = "long"}
+#' \code{\link{anansi}}, with \code{return.format = "table"}
 #'
 #' @return
 #' A ggplot2 object.
 #'
 #' @examples
-#' \dontrun{
 #' # Import libraries
 #' library(mia)
 #' library(TreeSummarizedExperiment)
@@ -107,11 +102,21 @@
 #' )
 #'
 #' # Select significant interactions
-#' out <- out[out$model_full_q.values < 0.1, ]
+#' out <- out[out$full_q.values < 0.1, ]
 #'
-#' # Generate dissociation plot
-#' plotAnansi(out, fill_by = "type", signif.threshold = 0.05)
-#' }
+#' # Visualise disjointed associations coloured by group
+#' plotAnansi(out,
+#'            association.type = "disjointed",
+#'            model.var = "Legend",
+#'            signif.threshold = 0.05,
+#'            fill_by = "group")
+#'            
+#' # Visualise full associations coloured by group
+#' plotAnansi(out,
+#'            association.type = "full",
+#'            signif.threshold = 0.05,
+#'            fill_by = "group")
+#'            
 #' @seealso
 #' \code{\link{getAnansi}}
 #' \code{\link{anansi}}
@@ -122,8 +127,8 @@ NULL
 
 #' @rdname plotAnansi
 #' @export
-setGeneric("plotAnansi", signature = c("df"),
-    function(df, ...) standardGeneric("plotAnansi")
+setGeneric("plotAnansi", signature = c("x"),
+    function(x, ...) standardGeneric("plotAnansi")
 )
 
 #' @rdname plotAnansi
@@ -132,60 +137,75 @@ setGeneric("plotAnansi", signature = c("df"),
 #'   scale_x_continuous scale_y_discrete scale_alpha_manual theme_bw
 #' @importFrom ggforce facet_col
 #' @importFrom stats setNames
-setMethod("plotAnansi", signature = c(df = "data.frame"),
-    function(df, x = "r.values", association.type = "disjointed", model.var = NULL,
-    colour_by = NULL, color_by = colour_by, fill_by = NULL, size_by = NULL,
-    shape_by = NULL, signif.threshold = NULL, y_position = "right",
+#' @importFrom S4Vectors isEmpty
+setMethod("plotAnansi", signature = c(x = "data.frame"),
+    function(x, association.type = NULL, model.var = NULL,
+    signif.threshold = NULL, colour_by = NULL, color_by = colour_by,
+    fill_by = NULL, size_by = NULL, shape_by = NULL, y_position = "right",
     x_lab = "cor", y_lab = ""){
-
-    match.arg(x, choices = c("r.values", "r.squared", "q.values"))
-    match.arg(association.type, choices = c("disjointed", "emergent", "full", "cor"))
-
-    if( association.type %in% c("disjointed", "emergent") && is.null(model.var) ){
+    # Check association.type
+    association_defined <- !is.null(association.type)
+    if( association_defined ){
+        match.arg(association.type, choices = c("disjointed", "emergent", "full"))
+    }
+    # Check model.var
+    var_defined <- !is.null(model.var)
+    if( var_defined ){
+        match.arg(model.var, attr(x, "model_terms"))
+    }
+    # Check association.type and model.var
+    if( association.type %in% c("disjointed", "emergent") && !var_defined ){
         stop("'model.var' must specify a variable of the anansi model when ",
             "'association type' is set to ", association.type, call. = FALSE)
     }
-    if( association.type == "full" && !is.null(model.var) ){
-        warning("'model.var' is ingored when 'association type' is set to ",
+    if( association.type == "full" && var_defined ){
+        model.var <- NULL
+        warning("'model.var' is ignored when 'association type' is set to ",
             association.type, call. = FALSE)
     }
-
-    pval <- paste0(c(association.type, model.var, "p.values"),
-        collapse = "_")
-    x <- paste0(c(association.type, model.var, x), collapse = "_")
-
-    # Check df
-    if( !all(c(x, pval, "feature_X", "feature_Y") %in% colnames(df)) ){
-        stop("'df' must be the output of 'getAnansi' in the long format and ",
-        "must contain columns '", x, "' ,'", pval, "', 'feature_X', 'feature_Y'",
+    # Derive p-value column from association.type and model.var
+    pval <- paste0(c(association.type, model.var, "p.values"), collapse = "_")
+    # Check x
+    if( isEmpty(x) ){
+        stop("'x' is an empty data.frame", call. = FALSE)
+    }
+    if( !all(c("feature_X", "feature_Y", pval) %in% colnames(x)) ){
+        stop("'x' must be the output of 'anansi' in the table format and must ",
+        "contain columns 'feature_X' ,'feature_Y', 'r.values' and '", pval, "'",
         call. = FALSE)
     }
-    # Check y_position
-    match.arg(y_position, choices = c("left", "right"))
+    # Convert anansi wide to long format
+    x <- .wide2long(x)
     # Update colour_by if color_by is defined
     if (!is.null(color_by) && is.null(colour_by)) {
         colour_by <- color_by
     }
     # Check aesthetics
-    colour_defined <- .check_aes(df, colour_by, "colour_by")
-    fill_defined <- .check_aes(df, fill_by, "fill_by")
-    size_defined <- .check_aes(df, size_by, "size_by")
-    shape_defined <- .check_aes(df, shape_by, "shape_by")
+    colour_defined <- .check_aes(x, colour_by, "colour_by")
+    fill_defined <- .check_aes(x, fill_by, "fill_by")
+    size_defined <- .check_aes(x, size_by, "size_by")
+    shape_defined <- .check_aes(x, shape_by, "shape_by")
     # Check signif.threshold
     signif_defined <- !is.null(signif.threshold)
-    if( signif_defined && !is.numeric(signif.threshold) ){
+    if( signif_defined && (!is.numeric(signif.threshold) || signif.threshold < 0 || signif.threshold > 1) ){
         stop("'signif.threshold' must be a number between 0 and 1", call. = FALSE)
     }
-    # Assemple plot data
-    pData <- data.frame(x = df[[x]], y = df[["feature_X"]],
-        colour = if (colour_defined) df[[colour_by]] else NA,
-        fill = if (fill_defined) df[[fill_by]] else NA,
-        size = if (size_defined) df[[size_by]] else NA,
-        shape = if (shape_defined) df[[shape_by]] else NA,
+    if( !association_defined && signif_defined ){
+        warning("'signif.threshold' is ignored when 'association type' is not",
+            " defined", call. = FALSE)
+    }
+    # Check y_position
+    match.arg(y_position, choices = c("left", "right"))
+    # Assemble plot data
+    pData <- data.frame(x = x[["r.values"]], y = x[["feature_X"]],
+        colour = if (colour_defined) x[[colour_by]] else NA,
+        fill = if (fill_defined) x[[fill_by]] else NA,
+        size = if (size_defined) x[[size_by]] else NA,
+        shape = if (shape_defined) x[[shape_by]] else NA,
         alpha = if (signif_defined)
-            factor(df[[pval]] < signif.threshold,
+            factor(x[[pval]] < signif.threshold,
             levels = c(TRUE, FALSE)) else NA,
-        facet = df[["feature_Y"]]
+        facet = x[["feature_Y"]]
     )
     # Create base plot
     p <- ggplot(data = pData, aes(x = .data$x, y = .data$y,
@@ -206,7 +226,7 @@ setMethod("plotAnansi", signature = c(df = "data.frame"),
         scale_x_continuous(limits = c(-1, 1), n.breaks = 11, expand = c(0, 0)) +
         scale_y_discrete(limits = rev, position = y_position)
     # Add significance legend
-    if( signif_defined ){
+    if( association_defined && signif_defined ){
         p <- p + scale_alpha_manual(values = c("TRUE" = 1, "FALSE" = 1/3),
             paste(association.type, "association\np <", signif.threshold))
     }
@@ -227,14 +247,29 @@ setMethod("plotAnansi", signature = c(df = "data.frame"),
 })
 
 ################################ HELP FUNCTIONS ################################
+# Convert anansi wide to long format
+#' @importFrom tidyr pivot_longer pivot_wider separate_wider_regex
+.wide2long <- function(x){
+    # Fetch group terms
+    group_terms <- attr(x, "group_terms")
+    # Create regex pattern to match group terms
+    patterns <- paste0("^(?:", paste(group_terms, collapse = "|"), ")")
+    # Convert anansi wide to long format
+    x_long <- x |>
+        pivot_longer(matches(patterns)) |>
+        separate_wider_regex(name,
+            c(group = patterns, "_", cor_param = ".\\.values$")) |>
+        pivot_wider(names_from = cor_param, values_from = value)
+    return(x_long)
+}
 # Check aesthetics
-.check_aes <- function(df, aes_var, aes_name) {
+.check_aes <- function(x, aes_var, aes_name){
     # Check if aesthetic is defined
     aes_defined <- !is.null(aes_var)
-    # Rise exception if aesthetic is not character or not in df
-    if( aes_defined && !(aes_var %in% colnames(df) && is.character(aes_var)) ){
+    # Rise exception if aesthetic is not character or not in x
+    if( aes_defined && !(aes_var %in% colnames(x) && is.character(aes_var)) ){
         stop("'", aes_name, "' must be a character string specifiying the",
-            " name of a column in 'df'", call. = FALSE)
+            " name of a column in 'x'", call. = FALSE)
     }
     return(aes_defined)
 }
